0d9a78b0a7ec321fd2be8b1475aa9007c25dfbde
crewch <artemkr2003@mail.ru>
2025-11-16 15:55:45 +0300

    Сделал 1 лабу

---
+++ lab1/1/lab1_1.m
@@ -0,0 +1,68 @@
+clear all; close all;
+
+%% 1. Загрузка изображения
+inputImagePath = 'Pic_22_1.bmp';
+originalImage = im2double(imread(inputImagePath));
+[height, width, ~] = size(originalImage);
+processedImage = originalImage;
+
+%% 1.1 Добавление прямоугольной сетки
+gridSpacing = 200; % Расстояние между линиями (пиксели)
+gridLineWidth = 3; % Толщина линий (пиксели)
+gridColor = [1, 1, 0]; % Цвет сетки [R, G, B]
+
+% Горизонтальные линии
+for row = 1:gridSpacing:height
+	endRow = min(row + gridLineWidth - 1, height);
+	processedImage(row:endRow, :, 1) = gridColor(1); % Красный канал
+	processedImage(row:endRow, :, 2) = gridColor(2); % Зелёный канал
+	processedImage(row:endRow, :, 3) = gridColor(3); % Синий канал
+end
+
+% Вертикальные линии
+for col = 1:gridSpacing:width
+	endCol = min(col + gridLineWidth - 1, width);
+	processedImage(:, col:endCol, 1) = gridColor(1);
+	processedImage(:, col:endCol, 2) = gridColor(2);
+	processedImage(:, col:endCol, 3) = gridColor(3);
+end
+
+%% 1.2 Добавление круглой области
+circleCenterY = 400; % Y-координата центра (строки)
+circleCenterX = 1000; % X-координата центра (столбцы)
+circleRadius = 150; % Радиус в пикселях
+
+% Проверка границ центра круга
+circleCenterY = max(1, min(circleCenterY, height));
+circleCenterX = max(1, min(circleCenterX, width));
+
+circleRadiusSq = circleRadius^2;
+
+for row = 1:height
+	% Вычисляем расстояние только для текущей строки
+	rowDistSq = (row - circleCenterY).^2 + ( (1:width) - circleCenterX ).^2;
+	inCircle = (rowDistSq <= circleRadiusSq);
+	
+	% Применяем изменения только в пределах изображения
+	validCols = inCircle & (1:width <= width);
+	
+	% Цвет
+	% processedImage(row, validCols, 1) = 0; % Красный канал
+	processedImage(row, validCols, 2) = 0; % Зелёный канал
+	processedImage(row, validCols, 3) = 0; % Синий канал
+end
+
+%% 1.3 Добавление прямоугольной области
+rectTop = 100; % Верхняя граница
+rectBottom = 500; % Нижняя граница
+rectLeft = 100; % Левая граница
+rectRight = 500; % Правая граница
+
+% Цвет
+processedImage(rectTop:rectBottom, rectLeft:rectRight, 1) = 0; % Красный
+processedImage(rectTop:rectBottom, rectLeft:rectRight, 2) = 0; % Зелёный
+% processedImage(rectTop:rectBottom, rectLeft:rectRight, 3) = 0; % Синий
+
+%% 1.4 Сохранение результата
+outputImagePath = './results/Processed_Pic_22_1.bmp';
+imwrite(processedImage, outputImagePath, 'bmp');
---
+++ lab1/2/lab1_2.m
@@ -0,0 +1,53 @@
+clear all; close all;
+
+%% Загрузка изображения
+inputImagePath = 'Pic_22_2.jpg';
+originalImage = im2double(imread(inputImagePath));
+[height, width, ~] = size(originalImage);
+
+%% Полутоновое изображение
+grayscaleImage = rgb2gray(originalImage);
+
+%% Негативное изображение
+negativeImage = 1 - grayscaleImage; % Инверсия значений пикселей
+
+%% Полутоновое с заданным числом градаций
+numGrayLevels = 10; % Заданное количество градаций серого (10 оттенков серого вместо 256)
+grayStep = 1 / numGrayLevels; % Шаг между уровнями яркости
+thresholds = 0:grayStep:1; % Пороговые значения для квантования
+quantizedImage = grayscaleImage; % Создание копии для модификации
+
+% Применение квантования к каждому пикселю
+for row = 1:height
+	for col = 1:width
+		for levelIdx = 2:length(thresholds)
+			currentPixel = quantizedImage(row, col);
+			lowerBound = thresholds(levelIdx-1);
+			upperBound = thresholds(levelIdx);
+			
+			% Проверка принадлежности пикселя к текущему диапазону
+			if currentPixel >= lowerBound && currentPixel < upperBound
+				quantizedImage(row, col) = lowerBound;
+			end
+		end
+	end
+end
+
+% Нормализация для максимального контраста
+maxPixelValue = max(quantizedImage(:));
+if maxPixelValue > 0
+	quantizedImage = quantizedImage / maxPixelValue;
+end
+
+%% Палитровое изображение
+% Формирование индексов для цветовой карты (диапазон 1-10)
+paletteIndices = round(10 * quantizedImage);
+paletteIndices = uint8(max(1, min(10, paletteIndices))); % Гарантирует, что индексы лежат в диапазоне [1, 10]
+springColormap = spring(10); % Получение цветовой схемы
+
+%% Сохранение всех результатов в файлы
+imwrite(originalImage, './results/original_color.jpg'); % Исходное цветное изображение
+imwrite(grayscaleImage, './results/grayscale.jpg'); % Полутоновое изображение
+imwrite(negativeImage, './results/negative.jpg'); % Негативное изображение
+imwrite(quantizedImage, './results/quantized.jpg'); % Полутоновое с заданным числом градаций
+imwrite(ind2rgb(paletteIndices, springColormap), './results/pseudocolored.jpg'); % Результат с цветовой картой
